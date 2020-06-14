d1747790e21ae1f69f8c109fc31d1787165518c3
Estuardo-Sazo <estuardosazo1206@gmail.com>
2020-06-14 14:46:57 -0600

    Add

--- index.php
+++ index.php
@@ -1,1 +1,61 @@
-<h1>Hola</h1>
+<?php
+
+/// Metodos de impresion
+echo 'Hola ', 'HOLA ',2324;
+echo '<br>';
+print 'HOla Mundo'; 
+echo '<br>';
+
+// Tipos de strgin
+
+$animal= "Panda";
+
+echo "Hola $animal ";
+echo '<br>';
+echo " \"Hola $animal\" ";
+
+echo '<br>';
+echo 'Hola $animal';
+
+echo '<br>';
+echo <<<FRASE
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit provident ratione molestiae eius sit quidem laborum ab enim vero temporibus, soluta officia? Tenetur eligendi obcaecati odio nostrum debitis? Ex, delectus!
+
+FRASE;
+
+/// Conversiones y tiopos de variales
+echo '<br>';
+$var="Gato";
+$var1=123;
+echo gettype($var);
+echo '<br>';
+echo gettype($var1);
+echo '<br>';
+
+$price=2029.50;
+$age=12;
+$name='Ozzy';
+$isDog=true;
+
+echo '<br>';
+echo gettype($price);
+echo '<br>';
+echo gettype($age);
+echo '<br>';
+echo gettype($name);
+echo '<br>';
+echo gettype($isDog);
+
+
+echo '<br>';
+echo var_dump($price);
+echo '<br>';
+echo var_dump($age);
+echo '<br>';
+echo var_dump($name);
+echo '<br>';
+echo var_dump($isDog);
+
+
+?>
